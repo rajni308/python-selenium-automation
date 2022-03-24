307a062ff78627f21e2034c91a66a0506de56bbd
Rajni Kandekar <rajni@rajnis-mbp.lan>
2022-03-24 19:17:25 -0400

    searcg clear
    exit
    r

--- sample_script.py
+++ sample_script.py
@@ -1,3 +1,6 @@
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,13 +14,17 @@ driver.get('https://www.google.com/')
 
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Dress')
+search.send_keys('dress')
 
 # wait for 4 sec
-sleep(4)
+# sleep(4)
+driver.wait = WebDriverWait(driver, 10)
 
 # click search
-driver.find_element(By.NAME, 'btnK').click()
+# driver.find_element(By.NAME, 'btnK').click()
+
+element = driver.wait.until(EC.element_to_be_clickable((By.NAME, 'btnK')), message='Search button not found')
+element.click()
 
 # verify
 assert 'dress' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
